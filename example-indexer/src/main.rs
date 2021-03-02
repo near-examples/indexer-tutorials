b7a22a728fe1a67fe4c37531686fffa4d5ee0e79
Bohdan Khorolets <bogdan@khorolets.com>
2021-03-02 16:22:58 +0200

    Update the code according to last nearcore updates. Fix formatting

--- example-indexer/src/main.rs
+++ example-indexer/src/main.rs
@@ -2,7 +2,8 @@ fn main() {
     let args: Vec<String> = std::env::args().collect();
     let home_dir = std::path::PathBuf::from(near_indexer::get_default_home());
 
-    let command = args.get(1)
+    let command = args
+        .get(1)
         .map(|arg| arg.as_str())
         .expect("You need to provide a command: `init` or `run` as arg");
 
@@ -19,27 +20,24 @@ fn main() {
                 download_genesis_url: None,
             };
             near_indexer::indexer_init_configs(&home_dir, config_args);
-        },
+        }
         "run" => {
             let indexer_config = near_indexer::IndexerConfig {
-        home_dir: std::path::PathBuf::from(near_indexer::get_default_home()),
-        sync_mode: near_indexer::SyncModeEnum::FromInterruption,
-        await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::WaitForFullSync,
-    };
-
-    actix::System::builder()
-        .stop_on_panic(true)
-        .run(move || {
-            let indexer = near_indexer::Indexer::new(indexer_config);
-            let stream = indexer.streamer();
-            actix::spawn(listen_blocks(stream));
-        })
-        .unwrap();
+                home_dir: std::path::PathBuf::from(near_indexer::get_default_home()),
+                sync_mode: near_indexer::SyncModeEnum::FromInterruption,
+                await_for_node_synced: near_indexer::AwaitForNodeSyncedEnum::WaitForFullSync,
+            };
+            let sys = actix::System::new();
+            sys.block_on(async move {
+                let indexer = near_indexer::Indexer::new(indexer_config);
+                let stream = indexer.streamer();
+                actix::spawn(listen_blocks(stream));
+            });
+            sys.run().unwrap();
         }
         _ => panic!("You have to pass `init` or `run` arg"),
     }
 }
-
 
 async fn listen_blocks(mut stream: tokio::sync::mpsc::Receiver<near_indexer::StreamerMessage>) {
     while let Some(streamer_message) = stream.recv().await {
